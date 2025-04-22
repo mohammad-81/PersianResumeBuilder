f1ee8b6e30d7df43a1e0718eaa27b4f14330bc3b
mohammad-81 <mmdsallamat2002@gmail.com>
2025-04-22 12:15:35 +0330

    Add project files.

---
+++ PersianResumeBuilder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersianResumeBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
