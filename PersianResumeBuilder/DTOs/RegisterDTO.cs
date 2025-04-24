366e49c2af5b6e7cbf24469058fbe8675127f495
mohammad-81 <mmdsallamat2002@gmail.com>
2025-04-24 13:05:44 +0330

    Add-RegisterDto to DTOs Folder for use Regeister

---
+++ PersianResumeBuilder/DTOs/RegisterDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersianResumeBuilder.DTOs
+{
+    public class RegisterDTO
+    {
+        public string FullName { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="رمز وارد شده یکسان نیست")]
+        public string Re_Password { get; set; }
+
+
+    }
+}
--- PersianResumeBuilder/Entities/User.cs
+++ PersianResumeBuilder/Entities/User.cs
@@ -3,11 +3,9 @@
     public class User
     {
         public int UserId { get; set; }
-
         public string FullName { get; set; }
         public int Phone { get; set; }
         public string Email { get; set; }
-
         public string Password { get; set; }
 
 
